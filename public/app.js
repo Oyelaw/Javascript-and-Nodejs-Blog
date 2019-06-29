61a4627bbdaae4427eefec030a5857cf9ec42093
okiki <okiki.oyelaw@gmail.com>
2019-06-29 10:34:38 +0100

    dynamically load post view page

--- public/app.js
+++ public/app.js
@@ -58,88 +58,67 @@ app.loadIndexPage = function () {
     });
 };
 
-// // Load Post view Page
-// app.loadPostViewPage = function () {
-//   // // Grab Post Id from window location
-//   // var id = typeof(window.location.href.split('=')[1]) == 'string' && window.location.href.split('=')[1].length > 0 ? window.location.href.split('=')[1] : false;
-//   //
-//   // if (id) {
-//   //   var queryStringObject = {
-//   //     'id': id
-//   //   };
-//   //
-//   //   httpRequest.request(undefined, 'api/post', 'GET', queryStringObject, undefined)
-//   //     .then(function (responsePayload) {
-//   //       var { title, article, images } = responsePayload;
-//   //
-//   //       var postViewContainer = document.querySelector('.postViewContainer');
-//   //       var leftPaneDiv = createNode('div');
-//   //       var rightPaneDiv = createNode('div');
-//   //
-//   //       var articleParagraph = createNode('p');
-//   //       var mainPostImage = createNode('img');
-//   //
-//   //       var recentPostsDiv = createNode('div');
-//   //       var archievesDiv = createNode('div');
-//   //       var sideHeadersRecentPosts = createNode('h5');
-//   //       var sideHeadersArchieves = createNode('h5');
-//   //
-//   //       // Create Extra Image Tags
-//   //       var extraImage1 = createNode('img');
-//   //       var extraImage2 = createNode('img');
-//   //       var extraImage3 = createNode('img');
-//   //       var extraImage4 = createNode('img');
-//   //
-//   //       // Add css class
-//   //       addToClasslist(extraImage1, 'extraPostViewImage');
-//   //       addToClasslist(extraImage2, 'extraPostViewImage');
-//   //       addToClasslist(extraImage3, 'extraPostViewImage');
-//   //       addToClasslist(extraImage4, 'extraPostViewImage');
-//   //
-//   //       addToClasslist(mainPostImage, 'postViewImage');
-//   //       addToClasslist(leftPaneDiv, 'viewPostLeftPane');
-//   //       addToClasslist(articleParagraph, 'article');
-//   //
-//   //       sideHeadersRecentPosts.innerHTML = "recent posts";
-//   //       sideHeadersArchieves.innerHTML = "archieves";
-//   //
-//   //       var postImages = {};
-//   //       // create helper function for below
-//   //         postImages.images = typeof(responsePayload.images) == 'object' && responsePayload.images instanceof Array ? responsePayload.images : [];
-//   //         postImages.images[0] ? mainPostImage.src = postImages.images[0].src : mainPostImage.style.display = 'none' ;
-//   //         postImages.images[1] ? extraImage1.src = postImages.images[1].src : extraImage1.style.display = 'none' ;
-//   //         postImages.images[2] ? extraImage2.src  = postImages.images[2].src : extraImage2.style.display = 'none' ;
-//   //         postImages.images[3] ? extraImage3.src = postImages.images[3].src : extraImage3.style.display = 'none' ;
-//   //         postImages.images[4] ? extraImage4.src = postImages.images[4].src : extraImage4.style.display = 'none' ;
-//   //
-//   //         var articleTextNode = document.createTextNode(article);
-//   //         append(articleParagraph, articleTextNode);
-//   //         append(leftPaneDiv, mainPostImage);
-//   //         append(leftPaneDiv, articleParagraph);
-//   //         append(leftPaneDiv, extraImage1);
-//   //         append(leftPaneDiv, extraImage2);
-//   //         append(leftPaneDiv, extraImage3);
-//   //         append(leftPaneDiv, extraImage4);
-//   //
-//   //         append(postViewContainer, leftPaneDiv);
-//   //
-//   //         append(recentPostsDiv, sideHeadersRecentPosts);
-//   //         append(archievesDiv, sideHeadersArchieves);
-//   //
-//   //         // @TODO attach right pane to screen
-//   //         append(rightPaneDiv, recentPostsDiv);
-//   //         append(rightPaneDiv, archievesDiv);
-//   //
-//   //         // append(postViewContainer, rightPaneDiv);
-//       })
-//       .catch(function (err) {
-//         console.log('Error: ', err);
-//       })
-//
-//   } else {
-//     alert('Where do I go??')
-//   }
-// }
+// Load Post view Page
+app.loadPostViewPage = function () {
+  // // Grab Post Id from window location
+  var id = typeof(window.location.href.split('=')[1]) == 'string' && window.location.href.split('=')[1].length > 0 ? window.location.href.split('=')[1] : false;
+
+  if (id) {
+    var queryStringObject = {
+      'id': id
+    };
+
+    httpRequest.request(undefined, 'api/post', 'GET', queryStringObject, undefined)
+      .then(function (responsePayload) {
+        var { title, article, images } = responsePayload;
+
+        var leftPaneContent = document.querySelector('.left-pane-content');
+        var rightPaneContent = document.querySelector('.right-pane-content');
+
+        // Create Image Tags
+        var mainImage = createNode('img');
+        var extraImage1 = createNode('img');
+        var extraImage2 = createNode('img');
+        var extraImage3 = createNode('img');
+        var extraImage4 = createNode('img');
+
+        // Assign classes to images
+        addToClasslist(mainImage, 'postImage');
+        addToClasslist(extraImage1, 'postImage-extra');
+        addToClasslist(extraImage2, 'postImage-extra');
+        addToClasslist(extraImage3, 'postImage-extra');
+        addToClasslist(extraImage4, 'postImage-extra');
+
+
+        var articleParagraph = createNode('p');
+        var articleTextNode = document.createTextNode(article);
+        append(articleParagraph, articleTextNode);
+
+        var postImages = {};
+        // create helper function for below
+          postImages.images = typeof(responsePayload.images) == 'object' && responsePayload.images instanceof Array ? responsePayload.images : [];
+          postImages.images[0] ? mainImage.src = postImages.images[0].src : mainImage.style.display = 'none' ;
+          postImages.images[1] ? extraImage1.src = postImages.images[1].src : extraImage1.style.display = 'none' ;
+          postImages.images[2] ? extraImage2.src  = postImages.images[2].src : extraImage2.style.display = 'none' ;
+          postImages.images[3] ? extraImage3.src = postImages.images[3].src : extraImage3.style.display = 'none' ;
+          postImages.images[4] ? extraImage4.src = postImages.images[4].src : extraImage4.style.display = 'none' ;
+
+          append(leftPaneContent, mainImage);
+          append(leftPaneContent, articleParagraph);
+          append(leftPaneContent, extraImage1);
+          append(leftPaneContent, extraImage2);
+          append(leftPaneContent, extraImage3);
+          append(leftPaneContent, extraImage4);
+
+      })
+      .catch(function (err) {
+        console.log('Error: ', err);
+      })
+
+  } else {
+    alert('Where do I go??')
+  }
+}
 
 // Load data on page
 app.loadDataOnPage = function () {
@@ -151,9 +130,9 @@ app.loadDataOnPage = function () {
     app.loadIndexPage();
   }
 
-  // if(primaryClass == 'postView'){
-  //   app.loadPostViewPage();
-  // }
+  if(primaryClass == 'postView'){
+    app.loadPostViewPage();
+  }
 };
 
 app.init = function () {
